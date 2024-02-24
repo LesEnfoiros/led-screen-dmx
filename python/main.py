9b2c088453611872e1c6c1a986cd8742519381f2
Pythagus <34168890+Pythagus@users.noreply.github.com>
2024-02-24 10:41:52 +0100

    :art: Add assets

---
+++ python/main.py
@@ -0,0 +1,11 @@
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
+# Configuration for the matrix
+options = RGBMatrixOptions()
+options.rows = 64
+options.cols = 64
+options.chain_length = 1
+options.parallel = 1
+options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
+
+matrix = RGBMatrix(options = options)
